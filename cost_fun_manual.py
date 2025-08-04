57e7e5793df46f889c405c302d1b120ee52f1d30
Aboarab83 <97898954+Aboarab83@users.noreply.github.com>
2025-08-04 14:40:59 +0300

    Add files via upload

--- cost_fun_manual.py
+++ cost_fun_manual.py
@@ -5,13 +5,13 @@ x=np.array(range(1,6))
 y=np.array([5,8,11,14,17])
 iters=1000
 m=len(x) # m is the number of the samples
-jw_b=np.empty(iters)  #jw_b cost
-w=[0]
-b=[0]
-lr=0.001
+jw_b=np.empty(iters+1)  #jw_b cost
+w=np.zeros(iters+1)
+b=np.zeros(iters+1)
+lr=0.01
 
-for iter in range(iters):
-    f_w_b=w[iter]*x+b[iter]
+for iter in range(1,iters+1):
+    f_w_b=w[iter-1]*x+b[iter-1]
     jw_b[iter]=0
     dev_w=0
     dev_b=0
@@ -21,8 +21,7 @@ for iter in range(iters):
         dev_b+=y[i]-f_w_b[i]
         dev_w+=x[i]*(y[i]-f_w_b[i])
     jw_b[iter]/=m
-    w.append(w[iter]+lr*2*dev_w/m)
-    b.append(b[iter]+lr*2*dev_b/m)
-    if abs(jw_b[iter]-jw_b[iter-1])==0:
-        break
+    w[iter]=w[iter-1]+lr*2*dev_w/m
+    b[iter]=b[iter-1]+lr*2*dev_b/m
+
 print(w[-1],b[-1],f_w_b)
